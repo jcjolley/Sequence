5b3028274069e05acec789a06d459e6105b3aedd
Joshua Jolley <josh@jcjolley.com>
2020-02-16 17:03:56 -0700

    chore(documentation): Remove types on @params and @returns to conform with TSDoc

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export class Sequence<T> {
 
     /**
      * Creates a sequence from an argument-less generator function
-     * @param {() => Generator<T, void, undefined>} generatorFn
+     * @param generatorFn
      */
     constructor(generatorFn: () => Generator<T, void, undefined>) {
         if (typeof generatorFn === "function") {
@@ -19,8 +19,7 @@ export class Sequence<T> {
 
     /**
      * Creates a Sequence from an Iterable or a single object
-     * @param {Iterable<T> | T} obj
-     * @returns {Sequence<T>}
+     * @param obj
      */
     static of<T>(obj: T | Iterable<T>): Sequence<T> {
         if (isIterable<T>(obj)) {
@@ -38,8 +37,7 @@ export class Sequence<T> {
 
     /**
      * Creates a sequence from the provided arguments
-     * @param {T} items
-     * @returns {Sequence<T>}
+     * @param items
      */
     static ofItems<T>(...items: Array<T>): Sequence<T> {
         if (arguments.length === 1) {
@@ -53,7 +51,6 @@ export class Sequence<T> {
      * Creates a sequence from the entries of an object.
      * E.g., Sequence.fromObj({a: 1, b: 2}) is the same as Sequence.of([["a", 1], ["b", 2]])
      * @param obj
-     * @returns {Sequence<[string, unknown]>}
      */
     static fromObj(obj: any): Sequence<[string, unknown]> {
         const entries = Object.entries(obj);
@@ -68,10 +65,9 @@ export class Sequence<T> {
      * Create a range of numbers
      * E.g., Sequence.range() yields S[0, 1, 2, 3, ...]
      *   and Sequence.range(0, 10, 2) yields S[0, 2, 4, 6, 8]
-     * @param {number} start - defaults to 0
-     * @param {number} end - defaults to infinity
-     * @param {number} step - defaults to 1
-     * @returns {Sequence<number>}
+     * @param start - defaults to 0
+     * @param end - defaults to infinity
+     * @param step - defaults to 1
      */
     static range(start = 0, end = Infinity, step = 1): Sequence<number> {
         return new Sequence(function* () {
@@ -85,9 +81,8 @@ export class Sequence<T> {
 
     /**
      * Create a Sequence that repeats x a given number of times (defaults to infinity)
-     * @param {T} x
-     * @param {number} times
-     * @returns {Sequence<T>}
+     * @param x
+     * @param times
      */
     static repeat<T>(x: T, times = Infinity): Sequence<T> {
         return new Sequence(function* () {
@@ -104,7 +99,6 @@ export class Sequence<T> {
      * E.g., Sequence.iterate(x => x + 1, 5) yields S[6, 7, 8, 9, 10, ...]
      * @param fn
      * @param arg
-     * @returns {Sequence<any>}
      */
     static iterate(fn: any, arg) {
         return new Sequence(function* () {
@@ -119,8 +113,7 @@ export class Sequence<T> {
     /**
      * Creates a new sequence by apply the provided function to each element of a sequence
      * E.g., Sequence.ofItems(1,2,3).map(x => x + 1) yields S[2,3,4]
-     * @param {(x: T) => R} fn
-     * @returns {Sequence<R>}
+     * @param fn
      */
     map<R>(fn: (x: T) => R): Sequence<R> {
         const seq = this;
@@ -134,8 +127,7 @@ export class Sequence<T> {
     /**
      * Creates a new sequence by apply the provided function to each element of a sequence
      * E.g., Sequence.ofItems(0,1,2).mapIndexed((x, i) => x + i) yields S[0, 2, 4]
-     * @param {(x: T, index: number) => R} fn
-     * @returns {Sequence<R>}
+     * @param fn
      */
     mapIndexed<R>(fn: (x: T, index: number) => R): Sequence<R> {
         const seq = this;
@@ -151,8 +143,7 @@ export class Sequence<T> {
     /**
      * Calls the provided function for each item in the sequence, presumably for side effects
      * E.g., Sequence.ofItems(0,1,2).forEach(x => console.log(x)) will log each item in the sequence
-     * @param {(x: T, index: number) => R} fn
-     * @returns {Sequence<R>}
+     * @param fn
      */
     forEach(fn: (x: T) => any): void {
         for (const x of this) {
@@ -164,8 +155,7 @@ export class Sequence<T> {
      * Creates a sequence where each item in the sequence found as a key in the provided map is replaced by the
      * value for that key in the map.
      * E.g., Sequence.ofItems(1,2,3).replace(new Map([[1, "a"]]) yields S["a", 2, 3]
-     * @param {Map<T, R>} replacements
-     * @returns {Sequence<T | R>}
+     * @param replacements
      */
     replace<R>(replacements: Map<T, R>): Sequence<T | R> {
         const seq = this;
@@ -180,9 +170,8 @@ export class Sequence<T> {
      * Creates a sequence of elements split into groups the length of size (or smaller for the final group if
      * the sequence can't be split evenly.)
      * E.g., Sequence.range().take(7).chunk(3) yields S[S[0,1,2], S[3,4,5], S[6]]
-     * @param {number} size
-     * @param {number} step
-     * @returns {Sequence<Sequence<T>>}
+     * @param size
+     * @param step
      */
     chunk(size: number, step = 1): Sequence<Sequence<T>> {
         const seq = this;
@@ -210,7 +199,6 @@ export class Sequence<T> {
 
     /**
      * Returns the first item of a sequence, if it exists
-     * @returns {undefined | T}
      */
     first(): undefined | T {
         let gen = this.toGenerator();
@@ -220,7 +208,6 @@ export class Sequence<T> {
 
     /**
      * Returns the second item of a sequence, if it exists
-     * @returns {undefined | T}
      */
     second(): undefined | T {
         return this.nth(1)
@@ -228,8 +215,7 @@ export class Sequence<T> {
 
     /**
      * Returns the nth item of a sequence, if it exists
-     * @param {number} n
-     * @returns {undefined | T}
+     * @param n
      */
     nth(n: number): undefined | T {
         let gen = this.toGenerator();
@@ -249,8 +235,7 @@ export class Sequence<T> {
      * Creates a sequence from the current sequence, starting at `start`
      * E.g., Sequence.range().rest() yields S[1,2,3...]
      *   and Sequence.range().rest(3) yields S[3,4,5...]
-     * @param {number} start
-     * @returns {Sequence<T>}
+     * @param start
      */
     rest(start = 1): Sequence<T> {
         const seq = this;
@@ -270,8 +255,7 @@ export class Sequence<T> {
      * Alias for rest, Creates a sequence by dropping `n` elements from the current sequence
      * E.g., Sequence.range().drop(1) yields S[1,2,3...]
      *   and Sequence.range().drop(3) yields S[3,4,5...]
-     * @param {number} n
-     * @returns {Sequence<T>}
+     * @param n
      */
     drop(n: number): Sequence<T> {
         return this.rest(n);
@@ -280,8 +264,7 @@ export class Sequence<T> {
     /**
      * Creates a sequence by dropping elements while pred returns true
      * E.g., Sequence.range().dropWhile(x => x < 5) yields S[5,6,7,...]
-     * @param {(x: T) => boolean} pred
-     * @returns {Sequence<T>}
+     * @param pred
      */
     dropWhile(pred: (x: T) => boolean): Sequence<T> {
         const seq = this;
@@ -296,9 +279,8 @@ export class Sequence<T> {
     /**
      * Reduces a sequence into a single value using the provided function
      * E.g., Sequence.range().take(4).reduce((sum, x) => sum + x, 0) yields 6
-     * @param {(acc: any, x: T) => any} fn
+     * @param fn
      * @param initial
-     * @returns {any}
      */
     reduce(fn: (acc: any, x: T) => any, initial?: any) {
         let acc;
@@ -317,8 +299,7 @@ export class Sequence<T> {
      * Reduces a sequence into a single value using the provided function.  This is the same as @link{Sequence.reduce}
      * but uses the first item of the sequence as the initial value
      * E.g., Sequence.range().take(4).fold((sum, x) => sum + x) yields 6
-     * @param {(acc: any, x: T) => any} fn
-     * @returns {T}
+     * @param fn
      */
     fold(fn: (acc: any, x: T) => any) {
         let acc = this.first();
@@ -343,8 +324,7 @@ export class Sequence<T> {
      * Creates a sequence by taking `n` elements from the current sequence (or as many as there are to take up to n)
      * E.g., Sequence.range().take(5) yields S[0,1,2,3,4]
      *   and Sequence.ofItems(1,2,3).take(5) yields S[1,2,3]
-     * @param {number} n
-     * @returns {Sequence<T>}
+     * @param n
      */
     take(n: number): Sequence<T> {
         const seq = this;
@@ -364,8 +344,7 @@ export class Sequence<T> {
     /**
      * Creates a sequence by taking elements from the current sequence while `pred` returns true
      * E.g., Sequence.range().takeWhile(x => x < 5) yields S[0,1,2,3,4]
-     * @param {(x: T) => boolean} pred
-     * @returns {Sequence<T>}
+     * @param pred
      */
     takeWhile(pred: (x: T) => boolean): Sequence<T> {
         const seq = this;
@@ -384,8 +363,7 @@ export class Sequence<T> {
      * Creates a sequence by taking the last n elements of a sequence
      * CAUTION: This runs in linear time and will not return for infinite sequences
      * E.g., Sequence.range().take(10).takeLast(3) yields S[7,8,9]
-     * @param {number} n
-     * @returns {Sequence<T>}
+     * @param n
      */
     takeLast(n: number): Sequence<T> {
         const seq = this;
@@ -409,8 +387,7 @@ export class Sequence<T> {
     /**
      * Creates a sequence of every n elements
      * E.g., Sequence.range().takeNth(3) yields S[0,3,6,9,...]
-     * @param {number} n
-     * @returns {Sequence<T>}
+     * @param n
      */
     takeNth(n: number): Sequence<T> {
         const seq = this;
@@ -428,7 +405,6 @@ export class Sequence<T> {
     /**
      * Creates a sequence with every element of the current sequence but the last one.
      * E.g., Sequence.range().take(4).butLast() yields S[0,1,2]
-     * @returns {Sequence<T>}
      */
     butLast(): Sequence<T> {
         const seq = this;
@@ -450,8 +426,7 @@ export class Sequence<T> {
     /**
      * Creates a sequence with two elements from the current sequence split at n
      * E.g., Sequence.range().splitAt(3) yields S[S[0,1,2], S[3,4,5,...]]
-     * @param {number} n
-     * @returns {Sequence<Sequence<T>>}
+     * @param n
      */
     splitAt(n: number): Sequence<Sequence<T>> {
         return Sequence.ofItems(this.take(n), this.drop(n));
@@ -460,8 +435,7 @@ export class Sequence<T> {
     /**
      * Creates a sequence with two elements from the current sequence split at the first time pred returns false
      * E.g., Sequence.range().splitWith(x < 3) yields S[S[0,1,2], S[3,4,5,...]]
-     * @param {(x: T) => boolean} fn
-     * @returns {Sequence<Sequence<T>>}
+     * @param fn
      */
     splitWith(fn: (x: T) => boolean): Sequence<Sequence<T>> {
         return Sequence.ofItems(this.takeWhile(fn), this.dropWhile(fn));
@@ -478,8 +452,7 @@ export class Sequence<T> {
      * If string flattening is desired, set flattenStr to true
      * E.g., Sequence.of(["One", ["Two", "Three"], [["Four"], ["Five", "Six"]]]).flatten(true)
      * yields S["O","n","e","T","w","o","T","h","r","e","e","F","o","u","r","F","i","v","e","S","i","x"]
-     * @param {boolean} flattenStr
-     * @returns {Sequence<any>}
+     * @param flattenStr
      */
     flatten(flattenStr: boolean = false): Sequence<any> {
         const seq = this;
@@ -509,7 +482,6 @@ export class Sequence<T> {
      *
      * CAUTION: This uses a set internally and could cause memory issues if a large number of elements are consumed
      * from the sequence.  If using on infinite sets, consider dedupe instead
-     * @returns {Sequence<T>}
      */
     distinct(): Sequence<T> {
         const seq = this;
@@ -528,7 +500,6 @@ export class Sequence<T> {
      * Returns a sequence with no consecutive duplicates
      * E.g., Sequence.ofItems(1,1,2,2,3,3,1,1).dedupe() yields S[1,2,3,1]
      *
-     * @returns {Sequence<T>}
      */
     dedupe(): Sequence<T> {
         const seq = this;
@@ -546,8 +517,7 @@ export class Sequence<T> {
     /**
      * Similar to filter, but remove creates a sequence with all items matching pred *removed* from the sequence
      * E.g., Sequence.range().remove(x => x % 2 === 0) yields S[1,3,5,...]
-     * @param {(x: T) => boolean} pred
-     * @returns {Sequence<T>}
+     * @param pred
      */
     remove(pred: (x: T) => boolean): Sequence<T> {
         const seq = this;
@@ -563,8 +533,7 @@ export class Sequence<T> {
     /**
      * Prepends an item to the sequence
      * E.g., Sequence.range().prepend(-1) yields S[-1,0,1,2...]
-     * @param {T} x
-     * @returns {Sequence<T>}
+     * @param x
      */
     prepend(x: T): Sequence<T> {
         const seq = this;
@@ -577,8 +546,7 @@ export class Sequence<T> {
     /**
      * Appends an item to the sequence
      * E.g., Sequence.range(0,3).append(3) yields S[0,1,2,3]
-     * @param {T} x
-     * @returns {Sequence<T>}
+     * @param x
      */
     append(x: T): Sequence<T> {
         const seq = this;
@@ -593,8 +561,7 @@ export class Sequence<T> {
     /**
      * Creates a sequence by concatenating the provided iterable to the end of the sequence
      * E.g., Sequence.ofItems(1,2,3).concat([4,5,6]) yields S[1,2,3,4,5,6]
-     * @param {Iterable<T>} xs
-     * @returns {Sequence<T>}
+     * @param xs
      */
     concat(xs: Iterable<T>): Sequence<T> {
         const seq = this;
@@ -608,8 +575,7 @@ export class Sequence<T> {
      * Creates a sequence by applying a fn that returns an iterable to each item in the sequence and
      * successively concatenating the results
      * E.g., Sequence.of([1, 2, 3]).mapcat(x => [x * 2, x * 3]) yields S[2, 3, 4, 6, 6, 9]
-     * @param {(x: T) => Iterable<R>} fn
-     * @returns {Sequence<R>}
+     * @param fn
      */
     mapcat<R>(fn: (x: T) => Iterable<R>): Sequence<R> {
         const seq = this;
@@ -626,7 +592,6 @@ export class Sequence<T> {
     /**
      * Turns a finite sequence into an infinite sequence by repeating the sequence once it's been consumed.
      * E.g., Sequence.ofItems(1,2).cycle() yields S[1,2,1,2,1,2...]
-     * @returns {Sequence<T>}
      */
     cycle(): Sequence<T> {
         const seq = this;
@@ -648,8 +613,7 @@ export class Sequence<T> {
     /**
      * Interleaves the items of the provided iterable with the current sequence, terminating when either iterable terminates
      * E.g., Sequence.range().interleave(["a", "b", "c"]) yields S[0, "a", 1 "b", 2 "c"]
-     * @param {Iterable<R>} xs
-     * @returns {Sequence<T | R>}
+     * @param xs
      */
     interleave<R>(xs: Iterable<R>): Sequence<T | R> {
         const seq = this;
@@ -670,8 +634,7 @@ export class Sequence<T> {
     /**
      * Interposes a single item between every item of the sequence
      * E.g., Sequence.range().interpose("a") yields S[0, "a", 1, "a", 2 ,"a",...]
-     * @param {T} x
-     * @returns {Sequence<T>}
+     * @param x
      */
     interpose(x: T): Sequence<T> {
         const seq = this;
@@ -695,8 +658,7 @@ export class Sequence<T> {
     /**
      * Groups the elements of a sequence into lists based on the result of the provided fn.
      * E.g., Sequence.range().take(6).groupBy(x => x % 2 === 0) yields new Map([true, S[0, 2, 4]] [false, S[1,3,5]])
-     * @param {(x: T) => R} fn
-     * @returns {Map<R, Sequence<T>>}
+     * @param fn
      */
     groupBy<R>(fn: (x: T) => R): Map<R, Sequence<T>> {
         const map = new Map<R, Sequence<T>>();
@@ -716,8 +678,7 @@ export class Sequence<T> {
     /**
      * Partitions the sequence into two groups based on result from pred
      * E.g., Sequence.range().partition(x => x % 2 === 0) yields S[S[0, 2, 4,...], S[1, 3, 5,...]]
-     * @param {(x: T) => boolean} pred
-     * @returns {Sequence<Sequence<T>>}
+     * @param pred
      */
     partition(pred: (x: T) => boolean): Sequence<Sequence<T>> {
         return Sequence.ofItems(this.filter(pred), this.remove(pred))
@@ -727,8 +688,7 @@ export class Sequence<T> {
      * Partitions the sequence with the provided function into a new seq each time
      * E.g., Sequence.ofItems(1, 1, 2, 2).cycle().partitionBy(x => x % 2 === 1)
      * yields S[S[1, 1], S[2, 2], S[1, 1], ...]
-     * @param {(x: T) => any} fn
-     * @returns {Sequence<Sequence<T>>}
+     * @param fn
      */
     partitionBy(fn: (x: T) => any): Sequence<Sequence<T>> {
         const seq: Sequence<T> = this;
@@ -747,7 +707,6 @@ export class Sequence<T> {
      *  Removes falsey values, like 0, null, undefined, and false
      *
      *  @param voidOnly - if true, only removes "void" (null || undefined)
-     *  @returns A list with only the 'truthy' items remaining
      */
     compact(voidOnly = false): Sequence<T> {
         if (voidOnly) {
@@ -759,7 +718,6 @@ export class Sequence<T> {
 
     /**
      * Creates an Array from the sequence
-     * @returns {T[]}
      */
     toArray(): T[] {
         return [...this]
@@ -769,7 +727,6 @@ export class Sequence<T> {
      * Creates a Map from a sequence of key value pairs, or chunks the sequence by 2 and treats those as key value pairs
      * E.g., Sequence.of([["a", 1], ["b", 2]]).toMap() yields new Map([["a", 1], ["b", 2]])
      *   and Sequence.ofItems("a", 1, "b", 2").toMap() yields new Map([["a", 1], ["b", 2]])
-     * @returns {Map<K, V>}
      */
     toMap<K, V>(): Map<K, V> {
         const first = this.first();
@@ -787,7 +744,6 @@ export class Sequence<T> {
 
     /**
      * Creates a Set from the sequence
-     * @returns {Set<T>}
      */
     toSet(): Set<T> {
         return new Set(this);
@@ -795,7 +751,6 @@ export class Sequence<T> {
 
     /**
      * Creates an object from the sequence
-     * @returns {{[p: string]: any}}
      */
     toObject(): { [key: string]: any } {
         const first: T = this.first();
@@ -821,7 +776,6 @@ export class Sequence<T> {
 
     /**
      * Creates a string from the sequence
-     * @returns {string}
      */
     toString(): string {
         let res = "";
@@ -833,7 +787,6 @@ export class Sequence<T> {
 
     /**
      * Returns a generator for the seq
-     * @returns {Generator<T, void, undefined>}
      */
     toGenerator(): Generator<T, void, undefined> {
         return this[Symbol.iterator]();
@@ -841,7 +794,6 @@ export class Sequence<T> {
 
     /**
      * Checks if the set is empty
-     * @returns {boolean}
      */
     isEmpty(): boolean {
         const gen = this.toGenerator();
@@ -852,7 +804,6 @@ export class Sequence<T> {
     /**
      * Determines the length of the sequence in linear time
      * CAUTION: Do not use on an infinite sequence.
-     * @returns {number}
      */
     length(): number {
         let count = 0;
